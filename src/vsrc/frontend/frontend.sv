6953fb635a6ef612ebf7309c4bc9e3204554302a
Easton Man <manyang.me@outlook.com>
2022-05-04 15:11:02 +0800

    feat: polish frontend design

--- src/vsrc/frontend/frontend.sv
+++ src/vsrc/frontend/frontend.sv
@@ -1,13 +1,23 @@
 `include "instr_info.sv"
 module frontend #(
-    parameter FETCH_WIDTH = 2
+    parameter FETCH_WIDTH = 2,
+    parameter ADDR_WIDTH  = 32,
+    parameter DATA_WIDTH  = 32
 ) (
     input logic clk,
     input logic rst,
 
+    // <-> ICache
+    output logic [ADDR_WIDTH-1:0] icache_read_addr_o[FETCH_WIDTH],
+    input logic icache_stallreq_i,  // ICache cannot accept more addr input
+    input logic icache_read_valid_i[FETCH_WIDTH],
+    input logic [ADDR_WIDTH-1:0] icache_read_addr_i[FETCH_WIDTH],
+    input logic [DATA_WIDTH-1:0] icache_read_data_i[FETCH_WIDTH],
+
+
     // <-> Backend
     input branch_update_info_t branch_update_info_i,
-    input logic [`InstAddrBus] backend_next_pc_i,
+    input logic [ADDR_WIDTH-1:0] backend_next_pc_i,
     input logic backend_flush_i,
 
     // <-> Instruction buffer
