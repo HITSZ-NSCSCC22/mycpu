59bf6f1d70070526d7a53edd286eae0f0511398d
250HandsomeLiang <2502481961@qq.com>
2022-08-03 22:32:14 +0800

    initial background

--- peripheral/lcd_init.sv
+++ peripheral/lcd_init.sv
@@ -12,11 +12,16 @@ module lcd_init (
         output logic        init_work,
         output logic        init_finish
     );
-
+    logic refresh_flag;
+    logic [4:0]refresh_addra;
+    logic [15:0]refresh_data;
+    logic [31:0]refresh_counter;
+    logic is_color;
     enum int {
              IDLE,
              INIT,
              CLEAR,//to refresh the lcd
+             DRAW_BG,//to draw the background
              DRAW_LOGO,
              INIT_FINISH
          } init_state;
@@ -61,6 +66,10 @@ module lcd_init (
             rs <= 0;
             init_work <= 1;
             init_finish <= 0;
+            refresh_addra<=0;
+            refresh_flag<=0;
+            refresh_counter<=0;
+            is_color<=0;
         end
         else begin
             case (init_state)
@@ -77,13 +86,16 @@ module lcd_init (
                     if (init_write_ok) begin
                         //init finish
                         if (addra == 17'd783) begin
-                            init_state <= DRAW_LOGO;
+                            init_state <= CLEAR;
                             addra <= addra+1;
                             we <= 0;
                             wr <= 1;
                             rs <= 0;
                             init_work <= 1;
                             init_finish <= 0;
+                            refresh_addra<=0;
+                            refresh_flag<=1;
+                            refresh_counter<=0;
                         end
                         else begin
                             addra <= addra + 1;
@@ -108,9 +120,67 @@ module lcd_init (
                         init_finish <= 0;
                     end
                 end
+                //refresh LCD,draw white
+                CLEAR: begin
+                    if (init_write_ok) begin
+                        //write clear data
+                        if (refresh_addra == 18) begin
+                            init_state <= DRAW_BG;
+                            we <= 0;
+                            wr <= 1;
+                            rs <= 0;
+                            init_work <= 1;
+                            init_finish <= 0;
+                            refresh_flag<=1;
+                            refresh_addra<=0;
+                            is_color<=1;
+                            refresh_counter<=1;
+                        end
+                        else begin
+                            refresh_addra <= refresh_addra + 1;
+                            we <= 0;
+                        end
+                    end
+                    else begin
+                        refresh_addra<=refresh_addra;
+                        we <= 1;
+                        wr <= 1;
+                        if (addra[0] == 1)
+                            rs <= 1;
+                        else
+                            rs <= 0;
+                        init_work   <= 1;
+                        init_finish <= 0;
+                    end
+                end
+                //draw white,give the params to lcd
+                DRAW_BG: begin
+                    if(init_write_ok) begin
+                        if(refresh_counter==384000) begin
+                            init_state <= DRAW_LOGO;
+                            addra <= addra;
+                            we <= 0;
+                            wr <= 1;
+                            rs <= 0;
+                            init_work <= 1;
+                            init_finish <= 0;
+                            refresh_flag<=0;
+                            is_color<=0;
+                        end
+                        else begin
+                            we<=0;
+                            refresh_counter<=refresh_counter+1;
+                        end
+                    end
+                    else begin
+                        rs<=1;
+                        we <= 1;
+                        wr <= 1;
+                    end
+                end
                 DRAW_LOGO: begin
                     if (init_write_ok) begin
-                        //init finish
+                        //draw logo
                         if (addra == `INIT_END) begin
                             init_state <= INIT_FINISH;
                             addra <= 0;
@@ -136,8 +206,6 @@ module lcd_init (
                             rs <= 1;
                         else
                             rs <= 0;
-                        init_work   <= 1;
-                        init_finish <= 0;
                     end
                 end
                 INIT_FINISH: begin
@@ -162,6 +230,8 @@ module lcd_init (
         end
     end
     assign init_addra=addra;
+    logic [15:0]data_o;
+    assign init_data=refresh_flag?(is_color?16'hffff:refresh_data):data_o;
     //initial code
     lcd_init_bram u_lcd_init_bram (
                       .clka (pclk),      // input wire clka
@@ -169,8 +239,16 @@ module lcd_init (
                       .wea  (0),         // input wire [0 : 0] wea
                       .addra(addra),     // input wire [16 : 0] addra
                       .dina (0),         // input wire [15 : 0] dina
-                      .douta(init_data)  // output wire [15 : 0] douta
+                      .douta(data_o)  // output wire [15 : 0] douta
                   );
+    lcd_clear_bram your_instance_name (
+                       .clka(pclk),    // input wire clka
+                       .ena(1),      // input wire ena
+                       .wea(0),      // input wire [0 : 0] wea
+                       .addra(refresh_addra),  // input wire [4 : 0] addra
+                       .dina(0),    // input wire [15 : 0] dina
+                       .douta(refresh_data)  // output wire [15 : 0] douta
+                   );
 
     ila_2 lcd_init_debug (
               .clk(pclk), // input wire clk
